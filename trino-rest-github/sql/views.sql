7b2c89722fc9910023becd56bdbb18b5c79a8e39
Jan Was <jan.was@starburstdata.com>
2022-05-11 09:55:02 +0200

    [skip ci] Make unique views handle rows with same updated_at

--- trino-rest-github/sql/views.sql
+++ trino-rest-github/sql/views.sql
@@ -1,62 +1,56 @@
 CREATE OR REPLACE VIEW unique_pulls SECURITY INVOKER AS
 WITH latest AS (
-  SELECT owner, repo, id, max(updated_at) AS updated_at
+  SELECT *, row_number() OVER (PARTITION BY owner, repo, number ORDER BY updated_at DESC) AS rownum
   FROM pulls
-  GROUP BY owner, repo, id
 )
-SELECT p.*
-FROM pulls p
-JOIN latest ON (latest.owner, latest.repo, latest.id, latest.updated_at) = (p.owner, p.repo, p.id, p.updated_at);
+SELECT *
+FROM latest
+WHERE rownum = 1;
 
 CREATE OR REPLACE VIEW unique_pull_commits SECURITY INVOKER AS
 WITH latest AS (
-  SELECT owner, repo, pull_number, parent_shas, max(committer_date) AS committer_date
+  SELECT *, row_number() OVER (PARTITION BY owner, repo, pull_number, parent_shas ORDER BY committer_date DESC) AS rownum
   FROM pull_commits
-  GROUP BY owner, repo, pull_number, parent_shas
 )
-SELECT c.*
-FROM pull_commits c
-JOIN latest ON (latest.owner, latest.repo, latest.pull_number, latest.parent_shas, latest.committer_date) = (c.owner, c.repo, c.pull_number, c.parent_shas, c.committer_date);
+SELECT *
+FROM latest
+WHERE rownum = 1;
 
 CREATE OR REPLACE VIEW unique_pull_stats SECURITY INVOKER AS
 WITH latest AS (
-  SELECT owner, repo, pull_number, max(updated_at) AS updated_at
+  SELECT *, row_number() OVER (PARTITION BY owner, repo, pull_number ORDER BY updated_at DESC) AS rownum
   FROM pull_stats
-  GROUP BY owner, repo, pull_number
 )
-SELECT s.*
-FROM pull_stats s
-JOIN latest ON (latest.owner, latest.repo, latest.pull_number, latest.updated_at) = (s.owner, s.repo, s.pull_number, s.updated_at);
+SELECT *
+FROM latest
+WHERE rownum = 1;
 
 CREATE OR REPLACE VIEW unique_review_comments SECURITY INVOKER AS
 WITH latest AS (
-  SELECT owner, repo, id, max(updated_at) AS updated_at
+  SELECT *, row_number() OVER (PARTITION BY owner, repo, id ORDER BY updated_at DESC) AS rownum
   FROM review_comments
-  GROUP BY owner, repo, id
 )
-SELECT r.*
-FROM review_comments r
-JOIN latest ON (latest.owner, latest.repo, latest.id, latest.updated_at) = (r.owner, r.repo, r.id, r.updated_at);
+SELECT *
+FROM latest
+WHERE rownum = 1;
 
 CREATE OR REPLACE VIEW unique_issues SECURITY INVOKER AS
 WITH latest AS (
-  SELECT owner, repo, id, max(updated_at) AS updated_at
+  SELECT *, row_number() OVER (PARTITION BY owner, repo, id ORDER BY updated_at DESC) AS rownum
   FROM issues
-  GROUP BY owner, repo, id
 )
-SELECT i.*
-FROM issues i
-JOIN latest ON (latest.owner, latest.repo, latest.id, latest.updated_at) = (i.owner, i.repo, i.id, i.updated_at);
+SELECT *
+FROM latest
+WHERE rownum = 1;
 
 CREATE OR REPLACE VIEW unique_issue_comments SECURITY INVOKER AS
 WITH latest AS (
-  SELECT owner, repo, id, max(updated_at) AS updated_at
+  SELECT *, row_number() OVER (PARTITION BY owner, repo, id ORDER BY updated_at DESC) AS rownum
   FROM issue_comments
-  GROUP BY owner, repo, id
 )
-SELECT i.*
-FROM issue_comments i
-JOIN latest ON (latest.owner, latest.repo, latest.id, latest.updated_at) = (i.owner, i.repo, i.id, i.updated_at);
+SELECT *
+FROM latest
+WHERE rownum = 1;
 
 CREATE OR REPLACE VIEW latest_teams SECURITY INVOKER AS
 SELECT org, id, node_id, url, html_url, name, slug, description, privacy, permission, members_url, repositories_url, parent_id, parent_slug
